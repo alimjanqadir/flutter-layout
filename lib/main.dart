738336715fdc6317b38e50721b7398adba321c61
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2020-11-27 20:52:55 +0800

    Basic layout demo finished.

--- lib/main.dart
+++ lib/main.dart
@@ -13,109 +13,174 @@ class RowAndColumn extends StatelessWidget {
       home: Scaffold(
         appBar: null,
         body: ListView(children: [
-          // Row(
-          //     mainAxisSize: MainAxisSize.max,
-          //     mainAxisAlignment: MainAxisAlignment.start,
-          //     children: [Box(), Box(), Box()]),
-          // Divider(),
-          // Row(
-          //     mainAxisSize: MainAxisSize.max,
-          //     mainAxisAlignment: MainAxisAlignment.end,
-          //     children: [Box(), Box(), Box()]),
-          // Divider(),
-          // Row(
-          //     mainAxisSize: MainAxisSize.max,
-          //     mainAxisAlignment: MainAxisAlignment.center,
-          //     children: [Box(), Box(), Box()]),
-          // Divider(),
-          // Row(
-          //     mainAxisSize: MainAxisSize.max,
-          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //     children: [Box(), Box(), Box()]),
-          // Divider(),
-          // Row(
-          //     mainAxisSize: MainAxisSize.max,
-          //     mainAxisAlignment: MainAxisAlignment.spaceAround,
-          //     children: [Box(), Box(), Box()]),
-          // Divider(),
-          // Row(
-          //     mainAxisSize: MainAxisSize.max,
-          //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //     children: [Box(), Box(), Box()]),
-          // Divider(),
-          // Row(
-          //     mainAxisSize: MainAxisSize.max,
-          //     crossAxisAlignment: CrossAxisAlignment.start,
-          //     mainAxisAlignment: MainAxisAlignment.start,
-          //     children: [Box(), BiggerBox(), Box()]),
-          // Divider(),
-          // Row(
-          //     mainAxisSize: MainAxisSize.max,
-          //     crossAxisAlignment: CrossAxisAlignment.center,
-          //     mainAxisAlignment: MainAxisAlignment.start,
-          //     children: [Box(), BiggerBox(), Box()]),
-          // Divider(),
-          // Row(
-          //     mainAxisSize: MainAxisSize.max,
-          //     crossAxisAlignment: CrossAxisAlignment.end,
-          //     mainAxisAlignment: MainAxisAlignment.start,
-          //     children: [Box(), BiggerBox(), Box()]),
-          // Divider(),
-          // Row(children: [
-          //   Box(),
-          //   Flexible(fit: FlexFit.loose, child: Box()),
-          //   Flexible(flex: 1, fit: FlexFit.tight, child: Box()),
-          //   Flexible(flex: 1, fit: FlexFit.tight, child: Box()),
-          // ]),
-          // Divider(),
-          // Row(children: [
-          //   Box(),
-          //   Box(),
-          //   Box(),
-          //   Box(),
-          //   Box(),
-          //   Box(),
-          //   Box(),
-          //   Expanded(child: Box()),
-          //   Box(),
-          // ]),
-          // Divider(),
-          // Row(children: [
-          //   Box(),
-          //   Box(),
-          //   Box(),
-          //   SizedBox(width: 100, child: Box()),
-          //   SizedBox(width: 50),
-          //   Box(),
-          //   Box(),
-          // ]),
-          // Divider(),
-          // Row(children: [
-          //   Box(),
-          //   Spacer(flex: 1),
-          //   Box(),
-          // ]),
-          // Divider(),
-          Row(
-              crossAxisAlignment: CrossAxisAlignment.baseline,
-              textBaseline: TextBaseline.alphabetic,
-              children: [
-                Text("Test!",
-                    style: TextStyle(
-                        fontSize: 30,
-                        fontFamily: "Futura",
-                        color: Colors.blue)),
-                Text("Test!",
-                    style: TextStyle(
-                        fontSize: 50,
-                        fontFamily: "Futura",
-                        color: Colors.green)),
-                Text("Test!",
-                    style: TextStyle(
-                        fontSize: 40, fontFamily: "Futura", color: Colors.red)),
-              ]),
+          // _simpleBoxes(),
+          // _mainAxisSize(),
+          // _alignment(),
+          // _flexible(),
+          // _expanded(),
+          // _spacerAndSizedBox(),
+          // _text(),
+          // _icon(),
+          // _image()
         ]),
       ),
+    );
+  }
+
+  Widget _simpleBoxes() {
+    return Column(
+      children: [
+        Row(children: [Box(), Box(), Box()]),
+        Divider()
+      ],
+    );
+  }
+
+  Row _image() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.network(
+            'https://github.com/flutter/website/blob/master/examples/layout/sizing/images/pic3.jpg?raw=true')
+      ],
+    );
+  }
+
+  Widget _icon() {
+    return Column(
+      children: [
+        Row(
+          children: [Icon(Icons.widgets, size: 50, color: Colors.blue)],
+        ),
+        Divider(),
+      ],
+    );
+  }
+
+  Widget _text() {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text("test!",
+                style: TextStyle(
+                    fontSize: 30, fontFamily: "futura", color: Colors.blue)),
+            Text("test!",
+                style: TextStyle(
+                    fontSize: 50, fontFamily: "futura", color: Colors.green)),
+            Text("test!",
+                style: TextStyle(
+                    fontSize: 40, fontFamily: "futura", color: Colors.red)),
+          ],
+        ),
+        Divider(),
+      ],
+    );
+  }
+
+  Widget _spacerAndSizedBox() {
+    return Column(
+      children: [
+        Row(children: [
+          Box(),
+          Box(),
+          Box(),
+          SizedBox(width: 100, child: Box()),
+          SizedBox(width: 50),
+          Box(),
+          Box(),
+        ]),
+        Divider(),
+        Row(children: [
+          Box(),
+          Spacer(flex: 1),
+          Box(),
+        ]),
+        Divider(),
+      ],
+    );
+  }
+
+  Widget _expanded() {
+    return Column(
+      children: [
+        Row(children: [
+          Box(),
+          Box(),
+          Expanded(child: Box()),
+          Box(),
+        ]),
+        Divider(),
+      ],
+    );
+  }
+
+  Widget _flexible() {
+    return Column(
+      children: [
+        Row(children: [
+          Box(),
+          Flexible(flex: 0, fit: FlexFit.tight, child: Box()),
+          Flexible(flex: 3, fit: FlexFit.tight, child: Box()),
+        ]),
+        Divider(),
+      ],
+    );
+  }
+
+  Widget _alignment() {
+    return Column(
+      children: [
+        Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Box(), Box(), Box()]),
+        Divider(),
+        Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [Box(), Box(), Box()]),
+        Divider(),
+        Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [Box(), Box(), Box()]),
+        Divider(),
+        Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [Box(), Box(), Box()]),
+        Divider(),
+        Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [Box(), BiggerBox(), Box()]),
+        Divider(),
+        Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [Box(), BiggerBox(), Box()]),
+        Divider(),
+        Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [Box(), BiggerBox(), Box()]),
+        Divider(),
+      ],
+    );
+  }
+
+  Column _mainAxisSize() {
+    return Column(
+      children: [
+        Row(mainAxisSize: MainAxisSize.min, children: [Box(), Box(), Box()]),
+        Divider(),
+        Row(mainAxisSize: MainAxisSize.max, children: [Box(), Box(), Box()]),
+        Divider(),
+      ],
     );
   }
 }
